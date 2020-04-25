f85e6d0694a2fa1deaa2f5eecad7105b0d86bf21
asankov <a.sankov96@gmail.com>
2020-04-25 23:29:43 +0300

    Chapter 2: Add Exercise 2.7

--- chapter-2/2.7-lists/README.md
+++ chapter-2/2.7-lists/README.md
@@ -1,3 +1,22 @@
 ## Chapter 2: Algorithms and Data Structures
 
 ### 2.7 Lists
+
+#### Exercise 2-7
+Implement some of the other list operations: copy, merge, split, insert before of after specific item.
+How do the two insertion operations differ in difficulty? How much can you use the routines we've written,
+and how much must you create yourself?
+
+*Answer:* The solution can be found in `chapter-2/2.7-lists/lists.c`.
+- Copying - the `copy` function. That is simply straight forward as it initializes a new list and start adding
+copies of the element of the old list to the new one. For the purpose we can reuse the `newitem` function,
+which create a new element.
+- Merge - the `merge` function. In this we reuse `copy` to make copies of both lists and then just add them
+together via the `addend` function. Neat.
+- Insert before - the `insert_before` function. This one is a bit trickier, because there are 2 cases:
+    - when the element we want to insert before is the first one - in this case, we make a copy to the original list,
+    point the `next` field of the new element to this copy, and then point the original list pointer to the pointer of the new element.
+    - when the element we want to insert before is not the first one - in this case we point the `next` field of the new element to the `next` field of previous element of the list (of which we hold a copy), and then point the previous element of the list, of the new element. In both cases we don't reuse any of the existing functionality.
+- Insert after - the `insert_after` function. In this we iterate over the elements of the passed list, until we find the matching one.
+The make this point to the next from the original list, and the previous one points to the new element to be inserted.
+In this function we don't reuse any of the other ones, because it's just pointer changes and comparisons.
--- chapter-2/2.7-lists/lists.c
+++ chapter-2/2.7-lists/lists.c
@@ -11,13 +11,17 @@ struct Nameval
 };
 
 void freeall(Nameval *listp);
+Nameval *copy(Nameval *listp);
 void printnv(Nameval *p, void *arg);
 Nameval *newitem(char *name, int value);
 void incounter(Nameval *listp, void *arg);
 Nameval *lookup(Nameval *listp, char *name);
 Nameval *delitem(Nameval *listp, char *name);
+Nameval *merge(Nameval *list, Nameval *other);
 Nameval *addend(Nameval *listp, Nameval *newp);
 Nameval *addfront(Nameval *listp, Nameval *newp);
+int insert_after(Nameval *list, char *name, Nameval *newelem);
+int insert_before(Nameval *list, char *name, Nameval *newelem);
 void apply(Nameval *listp, void (*fn)(Nameval *, void *), void *arg);
 
 int main()
@@ -45,6 +49,41 @@ int main()
 
     list = delitem(list, "Ivan");
     apply(list, printnv, "%s: %d, ");
+    printf("\n");
+
+    list = addfront(list, newitem("Georgi", 3));
+    apply(list, printnv, "%s: %d, ");
+    printf("\n");
+
+    Nameval *newlist = copy(list);
+    printf("printing list\n");
+    apply(list, printnv, "%s: %d, ");
+    printf("\n");
+    printf("printing newlist\n");
+    apply(newlist, printnv, "%s: %d, ");
+    printf("\n");
+
+    list = delitem(list, "Gosho");
+    printf("printing list\n");
+    apply(list, printnv, "%s: %d, ");
+    printf("\n");
+    printf("printing newlist\n");
+    apply(newlist, printnv, "%s: %d, ");
+    printf("\n");
+
+    Nameval *merged = merge(list, newlist);
+    printf("printing merged\n");
+    apply(merged, printnv, "%s: %d, ");
+    printf("\n");
+
+    insert_after(merged, "Gosho", newitem("Gosho 2", 5));
+    printf("printing merged\n");
+    apply(merged, printnv, "%s: %d, ");
+    printf("\n");
+
+    insert_before(merged, "Georgi", newitem("Georgi 0.5", 5));
+    printf("printing merged\n");
+    apply(merged, printnv, "%s: %d, ");
     printf("\n");
 
     freeall(list);
@@ -134,6 +173,71 @@ void freeall(Nameval *listp)
         /* assumes name is freed elsewhere */
         free(listp);
     }
+}
+
+/* copy: copies the elements of listp into a new list, without modifying the original list
+    returns: the beginning of the new list
+*/
+Nameval *copy(Nameval *listp)
+{
+    Nameval *begin = NULL;
+
+    for (; listp != NULL; listp = listp->next)
+    {
+        Nameval *new = (Nameval *)malloc(sizeof(Nameval));
+        if (new == NULL)
+            return NULL; /* not enough memory to allocate new element*/
+
+        begin = addend(begin, newitem(listp->name, listp->value));
+    }
+
+    return begin;
+}
+
+/* merge: returns a new list that contains all elements from list and other, without modifying the original lists */
+Nameval *merge(Nameval *list, Nameval *other)
+{
+    return addend(copy(list), copy(other));
+}
+
+int insert_before(Nameval *list, char *name, Nameval *newelem)
+{
+    Nameval *prev = NULL;
+
+    for (; list != NULL; list = list->next)
+    {
+        if (strcmp(list->name, name) == 0)
+        {
+            if (prev == NULL) /* this means the matching element is the first one */
+            {
+                newelem->next = copy(list);
+                *list = *newelem;
+                return 1;
+            }
+
+            newelem->next = prev->next;
+            prev->next = newelem;
+            return 1;
+        }
+        prev = list;
+    }
+
+    return 0;
+}
+
+/* insert_after: it finds the elemenent with name=name in list and inserts newelem after that */
+int insert_after(Nameval *list, char *name, Nameval *newelem)
+{
+    for (; list != NULL; list = list->next)
+    {
+        if (strcmp(list->name, name) == 0)
+        {
+            newelem->next = list->next;
+            list->next = newelem;
+            return 1;
+        }
+    }
+    return 0;
 }
 
 void incounter(Nameval *listp, void *arg)
