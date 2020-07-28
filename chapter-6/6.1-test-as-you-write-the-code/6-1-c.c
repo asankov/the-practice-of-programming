b43f4c156c83cbca6fa4e7a46076254a2e399294
Anton Sankov <a.sankov96@gmail.com>
2020-07-28 10:51:12 +0300

    Chapter 6: Add Exercise 6-1.c

---
+++ chapter-6/6.1-test-as-you-write-the-code/6-1-c.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int test(char *src);
+void strcpy2(char *dest, char *src);
+
+int main()
+{
+    int res = 0;
+    res += test("");
+    res += test(NULL);
+    res += test("something");
+
+    if (res != 0)
+    {
+        printf("❌ %d tests failed\n", res);
+    }
+    else
+    {
+        printf("✅ all tests passed\n");
+    }
+
+    return res;
+}
+
+int test(char *src)
+{
+    char *n = (char *)malloc(100 * sizeof(char));
+    strcpy2(n, src);
+    return strcmp(n, src) != 0;
+}
+
+void strcpy2(char *dest, char *src)
+{
+    for (int i = 0; src[i] != '\0'; i++)
+        dest[i] = src[i];
+}
